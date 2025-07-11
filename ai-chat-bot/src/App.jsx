b46a8f87dbc004d782398780cf3e0772e936e67f
Akarsh Saraff <saraffakarsh@gmail.com>
2025-07-11 15:43:37 +0530

    commit

--- ai-chat-bot/src/App.jsx
+++ ai-chat-bot/src/App.jsx
@@ -1,8 +1,4 @@
-import FloatingSidebar from './components/FloatingSidebar.jsx'
 import Header from './components/Header.jsx'
-import FarmerIntro from './components/FarmerIntro.jsx'
-import ContactForm from './components/ContactForm.jsx'
-import DeliveryInstructions from './components/DeliveryInstructions.jsx'
 import ChatBot from './components/ChatBot.jsx'
 
 function App() {
