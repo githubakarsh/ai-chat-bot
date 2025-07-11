2b73e92e0c95b956cac3e73587e22712fe81a84f
Akarsh Saraff <saraffakarsh@gmail.com>
2025-07-11 11:56:55 +0530

    Add floating sidebar component

---
+++ ai-chat-bot/src/components/FloatingSidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './FloatingSidebar.css'
+
+function FloatingSidebar() {
+  return (
+    <div className="floating-sidebar">
+      <h3>Sidebar</h3>
+      <ul>
+        <li><a href="#">Item 1</a></li>
+        <li><a href="#">Item 2</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default FloatingSidebar
